3b243c845b11d4c66f79aaf4e84ec1faf6698f48
alexander11902 <avholstein@web.de>
2023-04-15 23:59:15 +0200

    +

--- A003_Aufgabenliste_Formular/script.js
+++ A003_Aufgabenliste_Formular/script.js
@@ -1,49 +1,80 @@
 "use strict";
-/*
-Aufgabe: L03_Aufgabenliste_DAtenstruktur
-Name: Alexander Holstein
-Matrikel: 271466
-Datum: 15.04.2023
-Quellen: Lars Riehle
-*/
-const inputTask = document.querySelector("#inputTask");
-const inputName = document.querySelector("#inputPerson");
-const inputDate = document.querySelector("#inputDate");
-const inputInfo = document.querySelector("#inputInfo");
-const addTaskBtn = document.querySelector("#addTaskBtn");
-const inputs = document.querySelectorAll("input");
-addTaskBtn.addEventListener(`click`, function () {
-    addTask();
-});
-function addTask() {
-    let Task = document.createElement("li");
-    let task = document.createElement("h1");
-    task.innerHTML = inputTask.value;
-    let name = document.createElement("p");
-    name.innerHTML = inputName.value;
-    let date = document.createElement("p");
-    date.innerHTML = inputDate.value;
-    let info = document.createElement("p");
-    info.innerHTML = inputInfo.value;
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = "delete";
-    deleteBtn.addEventListener(`click`, function () {
-        deleteTask(Task);
+var Aufgabenliste_datastructure;
+(function (Aufgabenliste_datastructure) {
+    const inputTask = document.querySelector("#inputTask");
+    const inputName = document.querySelector("#inputPerson");
+    const inputDate = document.querySelector("#inputDate");
+    const inputInfo = document.querySelector("#inputInfo");
+    const addTaskBtn = document.querySelector("#addTaskBtn");
+    const inputs = document.querySelectorAll("input");
+    // Überprüft, ob es bereits daten im speicher gibt
+    if (localStorage.getItem("tasks")) {
+        const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+        // durchläuft jede aufgabe
+        savedTasks.forEach(function (task) {
+            addTaskToList(task.task, task.name, task.date, task.info);
+        });
+    }
+    addTaskBtn.addEventListener('click', function () {
+        addTask();
     });
-    document.querySelector("#TaskList").appendChild(Task);
-    Task.appendChild(task);
-    Task.appendChild(name);
-    Task.appendChild(date);
-    Task.appendChild(info);
-    Task.appendChild(deleteBtn);
-    clearInput();
-}
-function clearInput() {
-    inputs.forEach(function (input) {
-        input.value = "";
-    });
-}
-function deleteTask(Task) {
-    Task.remove();
-}
+    function addTask() {
+        const task = inputTask.value;
+        const name = inputName.value;
+        const date = inputDate.value;
+        const info = inputInfo.value;
+        const taskObj = {
+            task: task,
+            name: name,
+            date: date,
+            info: info
+        };
+        addTaskToList(task, name, date, info);
+        // Speichert die aufgabe im lokalen speicher
+        if (localStorage.getItem("tasks")) {
+            const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+            savedTasks.push(taskObj);
+            localStorage.setItem("tasks", JSON.stringify(savedTasks));
+        }
+        else {
+            localStorage.setItem("tasks", JSON.stringify([taskObj]));
+        }
+        clearInput();
+    }
+    function addTaskToList(task, name, date, info) {
+        const Task = document.createElement("li");
+        const taskElem = document.createElement("h1");
+        taskElem.innerHTML = task;
+        const nameElem = document.createElement("p");
+        nameElem.innerHTML = name;
+        const dateElem = document.createElement("p");
+        dateElem.innerHTML = date;
+        const infoElem = document.createElement("p");
+        infoElem.innerHTML = info;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "delete";
+        deleteBtn.addEventListener('click', function () {
+            deleteTask(Task);
+        });
+        document.querySelector("#TaskList").appendChild(Task);
+        Task.appendChild(taskElem);
+        Task.appendChild(nameElem);
+        Task.appendChild(dateElem);
+        Task.appendChild(infoElem);
+        Task.appendChild(deleteBtn);
+    }
+    function clearInput() {
+        inputs.forEach(function (input) {
+            input.value = "";
+        });
+    }
+    function deleteTask(Task) {
+        //löscht die daten aus dem array
+        const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+        const taskIndex = Array.from(Task.parentNode.children).indexOf(Task);
+        savedTasks.splice(taskIndex, 1);
+        localStorage.setItem("tasks", JSON.stringify(savedTasks));
+        Task.remove();
+    }
+})(Aufgabenliste_datastructure || (Aufgabenliste_datastructure = {}));
 //# sourceMappingURL=script.js.map
--- A003_Aufgabenliste_Formular/script.ts
+++ A003_Aufgabenliste_Formular/script.ts
@@ -1,55 +1,98 @@
-/*
-Aufgabe: L03_Aufgabenliste_DAtenstruktur
-Name: Alexander Holstein
-Matrikel: 271466
-Datum: 15.04.2023
-Quellen: Lars Riehle
-*/
-const inputTask = document.querySelector("#inputTask") as HTMLInputElement;
-const inputName = document.querySelector("#inputPerson") as HTMLSelectElement;
-const inputDate = document.querySelector("#inputDate") as HTMLInputElement;
-const inputInfo = document.querySelector("#inputInfo") as HTMLInputElement;
-const addTaskBtn = document.querySelector("#addTaskBtn") as HTMLButtonElement;
 
-const inputs = document.querySelectorAll("input");
 
-addTaskBtn.addEventListener(`click`, function () {
-  addTask(); 
-})
+namespace Aufgabenliste_datastructure {
 
-function addTask() {
-  let Task: HTMLLIElement = document.createElement("li");
-  let task = document.createElement("h1");
-  task.innerHTML = inputTask.value;
-  let name: HTMLParagraphElement = document.createElement("p");
-  name.innerHTML = inputName.value;
-  let date: HTMLParagraphElement = document.createElement("p");
-  date.innerHTML = inputDate.value;
-  let info: HTMLParagraphElement = document.createElement("p");
-  info.innerHTML = inputInfo.value;
-  let deleteBtn: HTMLButtonElement = document.createElement("button");
-  deleteBtn.innerHTML = "delete";
-  deleteBtn.addEventListener(`click`, function () {
-    deleteTask(Task);
+
+
+  const inputTask = document.querySelector("#inputTask") as HTMLInputElement;
+  const inputName = document.querySelector("#inputPerson") as HTMLSelectElement;
+  const inputDate = document.querySelector("#inputDate") as HTMLInputElement;
+  const inputInfo = document.querySelector("#inputInfo") as HTMLInputElement;
+  const addTaskBtn = document.querySelector("#addTaskBtn") as HTMLButtonElement;
+
+  const inputs = document.querySelectorAll("input");
+
+  // Überprüft, ob es bereits daten im speicher gibt
+  if (localStorage.getItem("tasks")) {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks") as string);
+    // durchläuft jede aufgabe
+    savedTasks.forEach(function (task: any) {
+      addTaskToList(task.task, task.name, task.date, task.info);
+    });
+  }
+
+  addTaskBtn.addEventListener('click', function () {
+    addTask();
   })
 
-  document.querySelector("#TaskList")!.appendChild(Task);
+  function addTask() {
+    const task = inputTask.value;
+    const name = inputName.value;
+    const date = inputDate.value;
+    const info = inputInfo.value;
 
-  Task.appendChild(task);
-  Task.appendChild(name);
-  Task.appendChild(date);
-  Task.appendChild(info);
-  Task.appendChild(deleteBtn);
 
-  clearInput();
-}
 
-function clearInput() {
-  inputs.forEach(function (input: HTMLInputElement) {
-    input.value = "";
-  })
-}
+    const taskObj = {
+      task: task,
+      name: name,
+      date: date,
+      info: info
+    };
 
-function deleteTask(Task: HTMLLIElement) {
-  Task.remove();
+    addTaskToList(task, name, date, info);
+
+    // Speichert die aufgabe im lokalen speicher
+    if (localStorage.getItem("tasks")) {
+      const savedTasks = JSON.parse(localStorage.getItem("tasks") as string);
+      savedTasks.push(taskObj);
+      localStorage.setItem("tasks", JSON.stringify(savedTasks));
+    } else {
+      localStorage.setItem("tasks", JSON.stringify([taskObj]));
+    }
+
+    clearInput();
+  }
+
+  function addTaskToList(task: string, name: string, date: string, info: string) {
+    const Task: HTMLLIElement = document.createElement("li");
+    const taskElem = document.createElement("h1");
+    taskElem.innerHTML = task;
+    const nameElem: HTMLParagraphElement = document.createElement("p");
+    nameElem.innerHTML = name;
+    const dateElem: HTMLParagraphElement = document.createElement("p");
+    dateElem.innerHTML = date;
+    const infoElem: HTMLParagraphElement = document.createElement("p");
+    infoElem.innerHTML = info;
+    const deleteBtn: HTMLButtonElement = document.createElement("button");
+    deleteBtn.innerHTML = "delete";
+    deleteBtn.addEventListener('click', function () {
+      deleteTask(Task);
+    })
+
+    document.querySelector("#TaskList")!.appendChild(Task);
+
+    Task.appendChild(taskElem);
+    Task.appendChild(nameElem);
+    Task.appendChild(dateElem);
+    Task.appendChild(infoElem);
+    Task.appendChild(deleteBtn);
+  }
+
+  function clearInput() {
+    inputs.forEach(function (input: HTMLInputElement) {
+      input.value = "";
+    })
+  }
+
+  function deleteTask(Task: HTMLLIElement) {
+    //löscht die daten aus dem array
+    const savedTasks = JSON.parse(localStorage.getItem("tasks") as string);
+    const taskIndex = Array.from(Task.parentNode!.children).indexOf(Task);
+    savedTasks.splice(taskIndex, 1);
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+
+    Task.remove();
+  }
+
 }
