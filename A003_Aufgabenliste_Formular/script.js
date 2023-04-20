41a02b7c5b4e2ad1c9766347eb01e37f45dd085f
Alexander <avholstein@web.de>
2023-04-20 15:19:52 +0200

    +

--- A003_Aufgabenliste_Formular/script.js
+++ A003_Aufgabenliste_Formular/script.js
@@ -5,11 +5,31 @@ function addItem() {
     var item = document.createElement("li");
     var label = document.createElement("label");
     var checkbox = document.createElement("input");
+    var delButton = document.createElement("button");
+    var textItems = document.createElement("h6");
     checkbox.type = "checkbox";
+    checkbox.value = "hallo";
     label.appendChild(checkbox);
     label.appendChild(document.createTextNode(input.value));
     item.appendChild(label);
     list.appendChild(item);
+    textItems.appendChild(label);
+    label.appendChild(delButton);
     input.value = "";
+}
+window.addEventListener("load", finishedLoading);
+function finishedLoading() {
+    let infoText = "Die website hat geladen";
+    console.log(infoText);
+    let checkBox = document.querySelector("input");
+    let addItemButton = document.querySelector("#addItemButton");
+    checkBox.addEventListener("click", checkBoxWurdeGeklickt);
+    addItemButton.addEventListener("click", textboxwurdeangeklickt);
+}
+function checkBoxWurdeGeklickt() {
+    console.log("input wurde getriggert (checkbox)");
+}
+function textboxwurdeangeklickt() {
+    console.log("du befindest dich im textfeld");
 }
 //# sourceMappingURL=script.js.map
--- A003_Aufgabenliste_Formular/script.ts
+++ A003_Aufgabenliste_Formular/script.ts
@@ -1,15 +1,47 @@
 
 
 function addItem() {
-    var input: HTMLInputElement = <HTMLInputElement>document.getElementById("new-item");
-    var list: HTMLElement = <HTMLElement> document.getElementById("my-list");
-    var item = document.createElement("li");
-    var label = document.createElement("label");
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    label.appendChild(checkbox);
-    label.appendChild(document.createTextNode(input.value));
-    item.appendChild(label);
-    list.appendChild(item);
-    input.value = "";
-  }
+  var input: HTMLInputElement = <HTMLInputElement>document.getElementById("new-item");
+  var list: HTMLElement = <HTMLElement>document.getElementById("my-list");
+  var item = document.createElement("li");
+  var label = document.createElement("label");
+  var checkbox = document.createElement("input");
+  var delButton = document.createElement("button");
+  var textItems = document.createElement("h6");
+
+  checkbox.type = "checkbox";
+  checkbox.value = "hallo";
+  label.appendChild(checkbox);
+  label.appendChild(document.createTextNode(input.value));
+  item.appendChild(label);
+  list.appendChild(item);
+  textItems.appendChild(label);
+  label.appendChild(delButton);
+  input.value = "";
+
+}
+
+window.addEventListener("load", finishedLoading);
+
+function finishedLoading(): void {
+
+  let infoText = "Die website hat geladen";
+  console.log(infoText);
+  let checkBox: HTMLInputElement = <HTMLInputElement>document.querySelector("input");
+  let addItemButton: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#addItemButton");
+
+  checkBox.addEventListener("click", checkBoxWurdeGeklickt);
+  addItemButton.addEventListener("click", textboxwurdeangeklickt);
+
+}
+
+function checkBoxWurdeGeklickt(): void {
+
+  console.log("input wurde getriggert (checkbox)");
+
+}
+
+function textboxwurdeangeklickt(): void {
+
+  console.log("du befindest dich im textfeld");
+}
