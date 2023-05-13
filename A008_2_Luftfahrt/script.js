4b96dba0efc759272247de76e63d71263fbd0ccd
Alexander <avholstein@web.de>
2023-05-14 00:20:37 +0200

    +

--- A008_2_Luftfahrt/script.js
+++ A008_2_Luftfahrt/script.js
@@ -27,10 +27,20 @@ var A008_2_Luftfahrt;
         drawSun();
         drawClouds();
         drawMountain();
+        drawLandeplatz();
+        //drawWindhose()
+        drawTrees();
+        drawHouse();
+        drawPeople();
+    }
+    function randomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
     }
     // Hintergrund zeichnen
     function drawBackground() {
-        // Farbe und Höhe der Berge
         let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
         gradient.addColorStop(0, "lightblue");
         gradient.addColorStop(0.5, "white");
@@ -40,59 +50,26 @@ var A008_2_Luftfahrt;
         crc2.closePath();
         // Farbe und Höhe der Berge
         const mountainColor = "#a9a9a9";
-        const mountainHeight = canvasHeight / 4; // 1/4 der Höhe
+        const mountainHeight = (canvasHeight / 4) * 0.8; // 1/4 der Höhe
         // Anzahl der Berge
         const numMountains = 3;
         for (let i = 0; i < numMountains; i++) {
             // Zufällige Position und Breite des Berges
             const mountainX = Math.random() * canvasWidth;
-            const mountainWidth = Math.random() * (canvasWidth / 4) + canvasWidth / 8;
+            const mountainWidth = Math.random() * (canvasWidth / 5) + canvasWidth / 44;
+            // Verschiebung der Bergposition in die Mitte der Höhe des Canvas
+            const mountainY = (canvasHeight - mountainHeight) / 3;
             // Zeichnen des Berges
             crc2.beginPath();
-            crc2.moveTo(mountainX, (3 * canvasHeight) / 4); // 3/4 der Höhe
-            crc2.lineTo(mountainX + mountainWidth / 2, (3 * canvasHeight) / 4 - mountainHeight);
-            crc2.lineTo(mountainX + mountainWidth, (3 * canvasHeight) / 4);
+            crc2.moveTo(mountainX, mountainY + mountainHeight);
+            crc2.lineTo(mountainX + mountainWidth / 2, mountainY);
+            crc2.lineTo(mountainX + mountainWidth, mountainY + mountainHeight);
             crc2.closePath();
             crc2.fillStyle = mountainColor;
             crc2.fill();
         }
     }
     ;
-    // Hintergrund-Berge zeichnen
-    crc2.fillStyle = "gray";
-    crc2.beginPath();
-    crc2.moveTo(canvasWidth * 0.1, canvasHeight);
-    crc2.lineTo(canvasWidth * 0.5, canvasHeight * 0.4);
-    crc2.lineTo(canvasWidth * 0.9, canvasHeight);
-    crc2.closePath();
-    crc2.fill();
-    // Vordergrund-Bäume zeichnen
-    crc2.fillStyle = "darkgreen";
-    const treeSize = 30;
-    const treePositions = [
-        { x: canvasWidth * 0.15, y: canvasHeight * 0.9 },
-        { x: canvasWidth * 0.25, y: canvasHeight * 0.8 },
-        { x: canvasWidth * 0.35, y: canvasHeight * 0.9 },
-        { x: canvasWidth * 0.45, y: canvasHeight * 0.85 },
-        { x: canvasWidth * 0.55, y: canvasHeight * 0.95 },
-        { x: canvasWidth * 0.65, y: canvasHeight * 0.9 },
-        { x: canvasWidth * 0.75, y: canvasHeight * 0.85 },
-        { x: canvasWidth * 0.85, y: canvasHeight * 0.9 },
-    ];
-    treePositions.forEach((position) => {
-        crc2.fillRect(position.x - treeSize / 2, position.y - treeSize, treeSize, treeSize);
-    });
-    // Elliptischen Landeplatz zeichnen
-    const landingPadWidth = 150;
-    const landingPadHeight = 100;
-    const landingPadX = canvasWidth / 2 - landingPadWidth / 2;
-    const landingPadY = canvasHeight * 0.75 - landingPadHeight / 2;
-    crc2.fillStyle = "black";
-    crc2.ellipse(landingPadX + landingPadWidth / 2, landingPadY + landingPadHeight / 2, landingPadWidth / 2, landingPadHeight / 2, 0, 0, 2 * Math.PI);
-    crc2.fill();
-    // Kiosk zeichnen
-    crc2.fillStyle = "red";
-    crc2.fillRect(canvasWidth * 0.8, canvasHeight * 0.7, 50, 80);
     // Windhose zeichnen
     crc2.strokeStyle = "black";
     crc2.lineWidth = 2;
@@ -108,12 +85,6 @@ var A008_2_Luftfahrt;
     function getRandomDecimal() {
         return parseFloat((Math.random()).toFixed(1));
     }
-    function randomColor() {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-        return `rgb(${r}, ${g}, ${b})`;
-    }
     // Sonne im oberen rechten Rand zeichnen
     function drawSun() {
         let sunRadius = Math.min(40) + getRandomDecimal() * 100;
@@ -125,11 +96,46 @@ var A008_2_Luftfahrt;
         crc2.fill();
     }
     function drawMountain() {
-        crc2.fillStyle = "grey";
+        const mountainColor = "grey";
+        const mountainHeight = canvasHeight / 3; // Die Hälfte der Höhe des Canvas
+        // Position und Breite des Berges
+        const mountainWidth = canvasWidth / 4; // 1/4 der Breite des Canvas
+        const mountainX = 0 - mountainWidth + 175; // Linker Rand des Canvas
+        // Verschiebung der Bergposition auf 1/3 der Höhe des Canvas
+        const mountainY = (2 * canvasHeight) / 3; // 2/3 der Höhe des Canvas
+        // Zeichnen des Berges
         crc2.beginPath();
-        crc2.moveTo(0, canvasHeight / 3);
-        crc2.lineTo(canvasWidth / 4, canvasHeight / 1.8 + 120);
-        crc2.lineTo(canvasWidth - canvasWidth * 1, canvasHeight - 150);
+        crc2.moveTo(mountainX, mountainY);
+        crc2.lineTo(mountainX + mountainWidth / 2, mountainY - mountainHeight);
+        crc2.lineTo(mountainX + mountainWidth, mountainY);
+        crc2.closePath();
+        crc2.fillStyle = mountainColor;
+        crc2.fill();
+    }
+    function drawWindhose() {
+        // Position und Größe des Fahnenmasts
+        const poleX = canvasWidth / 2; // Horizontal zentriert
+        const poleY = canvasHeight; // Am unteren Rand des Canvas
+        const poleHeight = canvasHeight / 2; // Die Hälfte der Höhe des Canvas
+        // Position und Größe der Windhose
+        const whirlwindX = poleX; // Am Fahnenmast
+        const whirlwindY = poleY - poleHeight; // Oberhalb des Fahnenmasts
+        const whirlwindRadius = Math.min(canvasWidth, poleHeight) / 4; // Ein Viertel der kleineren Seitenlänge
+        // Anfangs- und Endwinkel der Windhose
+        const startAngle = 0;
+        const endAngle = 2 * Math.PI;
+        // Zeichnen des Fahnenmasts
+        crc2.fillStyle = "#a9a9a9"; // Farbe des Fahnenmasts
+        crc2.fillRect(poleX - 5, 0, 10, poleHeight);
+        // Zeichnen der Windhose
+        crc2.beginPath();
+        crc2.arc(whirlwindX, whirlwindY, whirlwindRadius, startAngle, endAngle);
+        crc2.closePath();
+        // Farbverlauf für die Windhose
+        const gradient = crc2.createRadialGradient(whirlwindX, whirlwindY, 0, whirlwindX, whirlwindY, whirlwindRadius);
+        gradient.addColorStop(0, "#ffffff"); // Innerer Bereich
+        gradient.addColorStop(1, "#a9a9a9"); // Äußerer Bereich
+        crc2.fillStyle = gradient;
         crc2.fill();
     }
     function drawClouds() {
@@ -139,10 +145,10 @@ var A008_2_Luftfahrt;
         const cloudHeight = 60;
         // Anzahl der Wolken
         const numClouds = 6;
-        const minX = 0; // 1/3 der Breite
-        const minY = 0; // 1/3 der Höhe
-        const maxX = (3 * canvasWidth) / 4; // 2/3 der Breite
-        const maxY = (canvasHeight * 1) / 3; // 2/3 der Höhe
+        const minX = 0;
+        const minY = 0;
+        const maxX = (3 * canvasWidth) / 4;
+        const maxY = (canvasHeight * 1) / 3;
         for (let i = 0; i < numClouds; i++) {
             // Zufällige Position der Wolken
             const cloudX = Math.random() * (maxX - minX) + minX;
@@ -155,6 +161,112 @@ var A008_2_Luftfahrt;
             crc2.fillStyle = cloudColor;
             crc2.fill();
             crc2.closePath();
+        }
+    }
+    function drawLandeplatz() {
+        // Position und Abmessungen der Ellipse
+        const ellipseX = canvasWidth / 2; // Horizontal zentriert
+        const ellipseY = (canvasHeight / 2) + (canvasHeight / 8); // Unterhalb der Mitte
+        const ellipseRadiusX = canvasWidth / 6; // Halbe Breite des Canvas
+        const ellipseRadiusY = canvasHeight / 22; // 1/8 der Höhe des Canvas
+        // Zeichnen der Ellipse
+        crc2.beginPath();
+        crc2.ellipse(ellipseX, ellipseY, ellipseRadiusX, ellipseRadiusY, 0, 0, 2 * Math.PI);
+        crc2.closePath();
+        crc2.fillStyle = "royalblue"; // Farbe der Ellipse
+        crc2.stroke();
+        crc2.fill();
+    }
+    function drawTrees() {
+        // Anzahl der Bäume
+        const numTrees = 3;
+        for (let i = 0; i < numTrees; i++) {
+            // Zufällige Position des Baums im unteren rechten Bereich
+            const treeX = Math.random() * (canvasWidth / 2);
+            const treeY = Math.random() * (canvasHeight / 2) + (canvasHeight / 1.4);
+            // Größe des Baums
+            const trunkWidth = 10 + Math.random() * 10;
+            const trunkHeight = 40 + Math.random() * 20;
+            const crownWidth = 80 + Math.random() * 50;
+            const crownHeight = 110 + Math.random() * 30;
+            // Zeichnen des Baumstamms
+            crc2.fillStyle = "#8b4513"; // Farbe des Baumstamms (braun)
+            crc2.fillRect(treeX - trunkWidth / 2, treeY - trunkHeight, trunkWidth, trunkHeight);
+            // Zeichnen der Baumkrone mit Bezierkurven
+            crc2.fillStyle = "#008000"; // Farbe der Baumkrone (grün)
+            crc2.beginPath();
+            crc2.moveTo(treeX, treeY - trunkHeight);
+            const controlPointOffsetX = crownWidth / 2;
+            const controlPointOffsetY = crownHeight / 2;
+            const crownTopY = treeY - trunkHeight - crownHeight;
+            crc2.bezierCurveTo(treeX - controlPointOffsetX, treeY - trunkHeight - controlPointOffsetY, treeX - controlPointOffsetX, crownTopY + controlPointOffsetY, treeX, crownTopY);
+            crc2.bezierCurveTo(treeX + controlPointOffsetX, crownTopY + controlPointOffsetY, treeX + controlPointOffsetX, treeY - trunkHeight - controlPointOffsetY, treeX, treeY - trunkHeight);
+            crc2.closePath();
+            crc2.fill();
+        }
+    }
+    function drawHouse() {
+        // Position des Hauses
+        const houseX = (4.5 / 6) * canvasWidth;
+        const houseY = (55 / 100) * canvasHeight;
+        // Größe des Hauses
+        const houseWidth = 80;
+        const houseHeight = 60;
+        // Zeichnen des Hauses
+        crc2.fillStyle = "#ff0000"; // Farbe des Hauses (rot)
+        crc2.fillRect(houseX, houseY, houseWidth, houseHeight);
+        // Zeichnen des Dachs
+        crc2.beginPath();
+        crc2.moveTo(houseX - 20, houseY);
+        crc2.lineTo(houseX + houseWidth + 20, houseY);
+        crc2.lineTo(houseX + houseWidth / 2, houseY - 40);
+        crc2.closePath();
+        crc2.fillStyle = "#800000"; // Farbe des Dachs (dunkelrot)
+        crc2.fill();
+    }
+    function drawPeople() {
+        // Anzahl der Strichmännchen
+        const numStickFigures = 5;
+        for (let i = 0; i < numStickFigures; i++) {
+            // Zufällige Position des Strichmännchens
+            const stickFigureX = Math.random() * canvasWidth;
+            const stickFigureY = Math.random() * canvasHeight;
+            // Zeichnen des Strichmännchens
+            crc2.strokeStyle = "#000000"; // Farbe des Strichmännchens (schwarz)
+            // Fallschirm
+            crc2.beginPath();
+            crc2.arc(stickFigureX, stickFigureY, 45, Math.PI * 1, 0);
+            crc2.closePath();
+            const gradient = crc2.createRadialGradient(stickFigureX, stickFigureY - 40, 0, stickFigureX, stickFigureY - 40, 30);
+            gradient.addColorStop(0, "teal"); // Innerer Farbstop (rot)
+            gradient.addColorStop(1, "black"); // Äußerer Farbstop (weiß)
+            crc2.fillStyle = gradient;
+            crc2.fill();
+            // Kopf
+            crc2.beginPath();
+            crc2.arc(stickFigureX, stickFigureY, 10, 0, Math.PI * 2);
+            crc2.closePath();
+            crc2.stroke();
+            // Körper
+            crc2.beginPath();
+            crc2.moveTo(stickFigureX, stickFigureY + 10);
+            crc2.lineTo(stickFigureX, stickFigureY + 40);
+            crc2.closePath();
+            crc2.stroke();
+            // Arme
+            crc2.beginPath();
+            crc2.moveTo(stickFigureX - 20, stickFigureY + 20);
+            crc2.lineTo(stickFigureX + 20, stickFigureY + 20);
+            crc2.closePath();
+            crc2.stroke();
+            // Beine
+            crc2.beginPath();
+            crc2.moveTo(stickFigureX, stickFigureY + 40);
+            crc2.lineTo(stickFigureX - 20, stickFigureY + 70);
+            crc2.moveTo(stickFigureX, stickFigureY + 40);
+            crc2.lineTo(stickFigureX + 20, stickFigureY + 70);
+            crc2.closePath();
+            crc2.stroke();
         }
     }
 })(A008_2_Luftfahrt || (A008_2_Luftfahrt = {}));
--- A008_2_Luftfahrt/script.ts
+++ A008_2_Luftfahrt/script.ts
@@ -37,13 +37,26 @@ namespace A008_2_Luftfahrt {
         drawSun();
         drawClouds();
         drawMountain();
+        drawLandeplatz();
+        //drawWindhose()
+        drawTrees();
+        drawHouse();
+        drawPeople();
+
+    }
+
+    function randomColor(): string {
+
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
 
     }
 
     // Hintergrund zeichnen
     function drawBackground() {
 
-        // Farbe und Höhe der Berge
 
         let gradient: CanvasGradient = crc2!.createLinearGradient(0, 0, 0, crc2!.canvas.height);
         gradient.addColorStop(0, "lightblue");
@@ -51,12 +64,12 @@ namespace A008_2_Luftfahrt {
         gradient.addColorStop(1, "#35682d");
 
         crc2!.fillStyle = gradient;
-        crc2.fillRect(0, 0, crc2!.canvas.width, crc2!.canvas.height);
+        crc2!.fillRect(0, 0, crc2!.canvas.width, crc2!.canvas.height);
         crc2!.closePath();
 
         // Farbe und Höhe der Berge
         const mountainColor = "#a9a9a9";
-        const mountainHeight = canvasHeight / 4; // 1/4 der Höhe
+        const mountainHeight = (canvasHeight / 4) * 0.8; // 1/4 der Höhe
 
         // Anzahl der Berge
         const numMountains = 3;
@@ -64,14 +77,18 @@ namespace A008_2_Luftfahrt {
         for (let i = 0; i < numMountains; i++) {
             // Zufällige Position und Breite des Berges
             const mountainX = Math.random() * canvasWidth;
-            const mountainWidth = Math.random() * (canvasWidth / 4) + canvasWidth / 8;
+            const mountainWidth = Math.random() * (canvasWidth / 5) + canvasWidth / 44;
+
+            // Verschiebung der Bergposition in die Mitte der Höhe des Canvas
+            const mountainY = (canvasHeight - mountainHeight) / 3;
 
             // Zeichnen des Berges
             crc2.beginPath();
-            crc2.moveTo(mountainX, (3 * canvasHeight) / 4); // 3/4 der Höhe
-            crc2.lineTo(mountainX + mountainWidth / 2, (3 * canvasHeight) / 4 - mountainHeight);
-            crc2.lineTo(mountainX + mountainWidth, (3 * canvasHeight) / 4);
+            crc2.moveTo(mountainX, mountainY + mountainHeight);
+            crc2.lineTo(mountainX + mountainWidth / 2, mountainY);
+            crc2.lineTo(mountainX + mountainWidth, mountainY + mountainHeight);
             crc2.closePath();
+
             crc2.fillStyle = mountainColor;
             crc2.fill();
         }
@@ -96,7 +113,7 @@ namespace A008_2_Luftfahrt {
         return parseFloat((Math.random()).toFixed(1));
     }
 
-    
+
     // Sonne im oberen rechten Rand zeichnen
     function drawSun() {
         let sunRadius = Math.min(40) + getRandomDecimal() * 100;
@@ -108,26 +125,65 @@ namespace A008_2_Luftfahrt {
         crc2!.fill();
     }
 
-    function drawMountain(){
+    function drawMountain() {
 
-        const mountainColor = "#a9a9a9";
-        const mountainHeight = canvasHeight / 2; // Die Hälfte der Höhe des Canvas
+        const mountainColor = "grey";
+        const mountainHeight = canvasHeight / 3; // Die Hälfte der Höhe des Canvas
 
         // Position und Breite des Berges
-        const mountainX = 0; // Linker Rand des Canvas
+
+
         const mountainWidth = canvasWidth / 4; // 1/4 der Breite des Canvas
+        const mountainX = 0 - mountainWidth + 175; // Linker Rand des Canvas
+
+        // Verschiebung der Bergposition auf 1/3 der Höhe des Canvas
+        const mountainY = (2 * canvasHeight) / 3; // 2/3 der Höhe des Canvas
 
         // Zeichnen des Berges
-        crc2!.beginPath();
-        crc2!.moveTo(mountainX, canvasHeight);
-        crc2!.lineTo(mountainX + mountainWidth / 2, canvasHeight - mountainHeight);
-        crc2!.lineTo(mountainX + mountainWidth, canvasHeight);
-        crc2!.closePath();
+        crc2.beginPath();
+        crc2.moveTo(mountainX, mountainY);
+        crc2.lineTo(mountainX + mountainWidth / 2, mountainY - mountainHeight);
+        crc2.lineTo(mountainX + mountainWidth, mountainY);
+        crc2.closePath();
 
-        crc2!.fillStyle = mountainColor;
-        crc2!.fill();
+        crc2.fillStyle = mountainColor;
+        crc2.fill();
     }
 
+    function drawWindhose(): void {
+
+        // Position und Größe des Fahnenmasts
+        const poleX = canvasWidth / 2; // Horizontal zentriert
+        const poleY = canvasHeight; // Am unteren Rand des Canvas
+        const poleHeight = canvasHeight / 2; // Die Hälfte der Höhe des Canvas
+
+        // Position und Größe der Windhose
+        const whirlwindX = poleX; // Am Fahnenmast
+        const whirlwindY = poleY - poleHeight; // Oberhalb des Fahnenmasts
+        const whirlwindRadius = Math.min(canvasWidth, poleHeight) / 4; // Ein Viertel der kleineren Seitenlänge
+
+        // Anfangs- und Endwinkel der Windhose
+        const startAngle = 0;
+        const endAngle = 2 * Math.PI;
+
+        // Zeichnen des Fahnenmasts
+        crc2.fillStyle = "#a9a9a9"; // Farbe des Fahnenmasts
+        crc2.fillRect(poleX - 5, 0, 10, poleHeight);
+
+        // Zeichnen der Windhose
+        crc2.beginPath();
+        crc2.arc(whirlwindX, whirlwindY, whirlwindRadius, startAngle, endAngle);
+        crc2.closePath();
+
+        // Farbverlauf für die Windhose
+        const gradient = crc2.createRadialGradient(whirlwindX, whirlwindY, 0, whirlwindX, whirlwindY, whirlwindRadius);
+        gradient.addColorStop(0, "#ffffff"); // Innerer Bereich
+        gradient.addColorStop(1, "#a9a9a9"); // Äußerer Bereich
+
+        crc2.fillStyle = gradient;
+        crc2.fill();
+
+    }
 
 
     function drawClouds(): void {
@@ -139,10 +195,10 @@ namespace A008_2_Luftfahrt {
 
         // Anzahl der Wolken
         const numClouds = 6;
-        const minX = 0; // 1/3 der Breite
-        const minY = 0; // 1/3 der Höhe
-        const maxX = (3 * canvasWidth) / 4; // 2/3 der Breite
-        const maxY = (canvasHeight * 1) / 3; // 2/3 der Höhe
+        const minX = 0;
+        const minY = 0;
+        const maxX = (3 * canvasWidth) / 4;
+        const maxY = (canvasHeight * 1) / 3;
 
         for (let i = 0; i < numClouds; i++) {
             // Zufällige Position der Wolken
@@ -174,6 +230,152 @@ namespace A008_2_Luftfahrt {
         }
     }
 
+    function drawLandeplatz() {
+
+        // Position und Abmessungen der Ellipse
+        const ellipseX = canvasWidth / 2; // Horizontal zentriert
+        const ellipseY = (canvasHeight / 2) + (canvasHeight / 8); // Unterhalb der Mitte
+        const ellipseRadiusX = canvasWidth / 6; // Halbe Breite des Canvas
+        const ellipseRadiusY = canvasHeight / 22; // 1/8 der Höhe des Canvas
+
+        // Zeichnen der Ellipse
+        crc2.beginPath();
+        crc2.ellipse(ellipseX, ellipseY, ellipseRadiusX, ellipseRadiusY, 0, 0, 2 * Math.PI);
+        crc2.closePath();
+
+        crc2.fillStyle = "royalblue"; // Farbe der Ellipse
+        crc2.stroke()
+        crc2.fill();
+    }
+
+    function drawTrees(): void {
+        // Anzahl der Bäume
+        const numTrees = 3;
+
+        for (let i = 0; i < numTrees; i++) {
+            // Zufällige Position des Baums im unteren rechten Bereich
+            const treeX = Math.random() * (canvasWidth / 2);
+            const treeY = Math.random() * (canvasHeight / 2) + (canvasHeight / 1.4);
+
+            // Größe des Baums
+            const trunkWidth = 10 + Math.random() * 10;
+            const trunkHeight = 40 + Math.random() * 20;
+            const crownWidth = 80 + Math.random() * 50;
+            const crownHeight = 110 + Math.random() * 30;
+
+            // Zeichnen des Baumstamms
+            crc2.fillStyle = "#8b4513"; // Farbe des Baumstamms (braun)
+            crc2.fillRect(treeX - trunkWidth / 2, treeY - trunkHeight, trunkWidth, trunkHeight);
+
+            // Zeichnen der Baumkrone mit Bezierkurven
+            crc2.fillStyle = "#008000"; // Farbe der Baumkrone (grün)
+            crc2.beginPath();
+            crc2.moveTo(treeX, treeY - trunkHeight);
+
+            const controlPointOffsetX = crownWidth / 2;
+            const controlPointOffsetY = crownHeight / 2;
+            const crownTopY = treeY - trunkHeight - crownHeight;
+
+            crc2.bezierCurveTo(treeX - controlPointOffsetX, treeY - trunkHeight - controlPointOffsetY,
+                treeX - controlPointOffsetX, crownTopY + controlPointOffsetY,
+                treeX, crownTopY);
+            crc2.bezierCurveTo(treeX + controlPointOffsetX, crownTopY + controlPointOffsetY,
+                treeX + controlPointOffsetX, treeY - trunkHeight - controlPointOffsetY,
+                treeX, treeY - trunkHeight);
+
+            crc2.closePath();
+            crc2.fill();
+        }
+    }
+
+    function drawHouse() {
+
+        // Position des Hauses
+        const houseX = (4.5 / 6) * canvasWidth;
+        const houseY = (55 / 100) * canvasHeight;
+
+        // Größe des Hauses
+        const houseWidth = 80;
+        const houseHeight = 60;
+
+        // Zeichnen des Hauses
+        crc2.fillStyle = "#ff0000"; // Farbe des Hauses (rot)
+        crc2.fillRect(houseX, houseY, houseWidth, houseHeight);
+
+        // Zeichnen des Dachs
+        crc2.beginPath();
+        crc2.moveTo(houseX - 20, houseY);
+        crc2.lineTo(houseX + houseWidth + 20, houseY);
+        crc2.lineTo(houseX + houseWidth / 2, houseY - 40);
+        crc2.closePath();
+        crc2.fillStyle = "#800000"; // Farbe des Dachs (dunkelrot)
+        crc2.fill();
+
+    }
 
 
+    function drawPeople() {
+        // Anzahl der Strichmännchen
+        const numStickFigures = 5
+
+        for (let i = 0; i < numStickFigures; i++) {
+            // Zufällige Position des Strichmännchens
+            const stickFigureX = Math.random() * canvasWidth;
+            const stickFigureY = Math.random() * canvasHeight;
+
+            // Zeichnen des Strichmännchens
+            crc2.strokeStyle = "#000000"; // Farbe des Strichmännchens (schwarz)
+
+             // Fallschirm
+             crc2.beginPath();
+             crc2.arc(stickFigureX, stickFigureY , 45,  Math.PI * 1,0);
+             crc2.closePath();
+ 
+             const gradient = crc2.createRadialGradient(stickFigureX, stickFigureY - 40, 0, stickFigureX, stickFigureY - 40, 30);
+             gradient.addColorStop(0, "teal"); // Innerer Farbstop (rot)
+             gradient.addColorStop(1, "black"); // Äußerer Farbstop (weiß)
+             crc2.fillStyle = gradient;
+             crc2.fill();
+
+            // Kopf
+            crc2.beginPath();
+            crc2.arc(stickFigureX, stickFigureY, 10, 0, Math.PI * 2);
+            crc2.closePath();
+            crc2.stroke();
+
+            // Körper
+            crc2.beginPath();
+            crc2.moveTo(stickFigureX, stickFigureY + 10);
+            crc2.lineTo(stickFigureX, stickFigureY + 40);
+            crc2.closePath();
+            crc2.stroke();
+
+            // Arme
+            crc2.beginPath();
+            crc2.moveTo(stickFigureX - 20, stickFigureY + 20);
+            crc2.lineTo(stickFigureX + 20, stickFigureY + 20);
+            crc2.closePath();
+            crc2.stroke();
+
+            // Beine
+            crc2.beginPath();
+            crc2.moveTo(stickFigureX, stickFigureY + 40);
+            crc2.lineTo(stickFigureX - 20, stickFigureY + 70);
+            crc2.moveTo(stickFigureX, stickFigureY + 40);
+            crc2.lineTo(stickFigureX + 20, stickFigureY + 70);
+            crc2.closePath();
+            crc2.stroke();
+
+           
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
