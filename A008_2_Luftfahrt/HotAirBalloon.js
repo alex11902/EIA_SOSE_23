523002048faf54f3f039078eded9756d000f42ed
Alexander <avholstein@web.de>
2023-06-24 22:46:43 +0200

    +

--- A008_2_Luftfahrt/HotAirBalloon.js
+++ A008_2_Luftfahrt/HotAirBalloon.js
@@ -3,17 +3,26 @@ var LuftfahrtPolymorphie;
 (function (LuftfahrtPolymorphie) {
     class HotAirBalloon extends LuftfahrtPolymorphie.Moveable {
         position;
-        velosity;
+        velocity;
+        isPopped;
         constructor() {
             super();
             console.log("Balloon constructor");
             this.position = new LuftfahrtPolymorphie.Vector(480, 400);
-            this.velosity = new LuftfahrtPolymorphie.Vector(0, 0);
-            this.velosity.random(100, 150);
+            this.velocity = new LuftfahrtPolymorphie.Vector(0, 0);
+            this.velocity.random(100, 150);
+            this.isPopped = false;
+            // Event Listener für den Klick auf den Ballon hinzufügen
+            //   let canvas =<HTMLCanvasElement>document.querySelector('#canvas');
+            //canvas.addEventListener("click", this.handleClick.bind(this));
         }
         move(_timeslice) {
-            //console.log("Balloon move");
-            let offset = new LuftfahrtPolymorphie.Vector(this.velosity.x, this.velosity.y);
+            if (this.isPopped) {
+                // Wenn der Ballon geplatzt ist, fällt er zum Boden
+                this.position.y += 5; // Geschwindigkeit des Falls anpassen
+                return;
+            }
+            let offset = new LuftfahrtPolymorphie.Vector(this.velocity.x, this.velocity.y);
             offset.scale(_timeslice * 0.2);
             this.position.add(offset);
             // Begrenzung auf y-Maximum
@@ -26,22 +35,25 @@ var LuftfahrtPolymorphie;
             // Begrenzung auf Canvas-Breite
             if (this.position.x < 0) {
                 this.position.x = 0;
-                this.velosity.x *= -1;
+                this.velocity.x *= -1;
             }
             else if (this.position.x > 820) {
                 this.position.x = 820;
-                this.velosity.x *= -1;
+                this.velocity.x *= -1;
             }
         }
         draw() {
-            //console.log("Balloon draw"); // Ballon
-            // Ballon
+            if (this.isPopped) {
+                // Wenn der Ballon geplatzt ist, zeichne nichts mehr
+                return;
+            }
+            // Zeichne den Ballon
             LuftfahrtPolymorphie.crc2.beginPath();
             LuftfahrtPolymorphie.crc2.arc(this.position.x, this.position.y, 25, 0, Math.PI * 2);
             LuftfahrtPolymorphie.crc2.fillStyle = "magenta";
             LuftfahrtPolymorphie.crc2.fill();
             LuftfahrtPolymorphie.crc2.closePath();
-            // Seile
+            // Zeichne die Seile
             LuftfahrtPolymorphie.crc2.beginPath();
             LuftfahrtPolymorphie.crc2.moveTo(this.position.x - 15, this.position.y + 20);
             LuftfahrtPolymorphie.crc2.lineTo(this.position.x - 5, this.position.y + 45);
@@ -51,12 +63,22 @@ var LuftfahrtPolymorphie;
             LuftfahrtPolymorphie.crc2.lineWidth = 1;
             LuftfahrtPolymorphie.crc2.stroke();
             LuftfahrtPolymorphie.crc2.closePath();
-            // Korb
+            // Zeichne den Korb
             LuftfahrtPolymorphie.crc2.beginPath();
             LuftfahrtPolymorphie.crc2.rect(this.position.x - 12.5, this.position.y + 45, 25, 20);
             LuftfahrtPolymorphie.crc2.fillStyle = "brown";
             LuftfahrtPolymorphie.crc2.fill();
             LuftfahrtPolymorphie.crc2.closePath();
+        }
+        handleClick(event) {
+            const clickX = event.clientX;
+            const clickY = event.clientY;
+            // Überprüfe, ob der Klick innerhalb des Ballons erfolgt ist
+            const distance = Math.sqrt((clickX - this.position.x) ** 2 + (clickY - this.position.y) ** 2);
+            if (distance <= 25) {
+                // Ballon platzen lassen, wenn er angeklickt wurde
+                this.isPopped = true;
+            }
         }
     }
     LuftfahrtPolymorphie.HotAirBalloon = HotAirBalloon;
